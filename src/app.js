653255d1f762ff6ac906b44ae97ddcdcf336a4de
Kyungseok <bestspx@gmail.com>
2025-12-26 15:56:17 +0900

    feat: 상세 정보 개발
    - 의존성 추가 (date-fns, notyf, tui-date-picker)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-import DatePicker from 'tui-date-picker'; /* ES6 */
+import {
+  addDays,
+  addHours,
+  clamp,
+  format,
+  isAfter,
+  isEqual,
+  setHours,
+  setMinutes,
+  subDays,
+} from 'date-fns';
+import { Notyf } from 'notyf';
+import DatePicker from 'tui-date-picker';
 
-console.log({ DatePicker });
+DatePicker.localeTexts['ko'] = {
+  ...DatePicker.localeTexts['ko'],
+  titleFormat: 'yyyy년 M월',
+};
 
 const FILE_SIZE_LIMIT = 15 * 10e6; // 15MB
 const FILE_SIZE_OVER_MSG = '15MB 이하 이미지를 업로드해 주세요';
@@ -17,13 +32,43 @@ const AM_PM_LABELS = {
   am: '오전',
   pm: '오후',
 };
+const UPDATE_TIME_TRIGGERS = {
+  START_TOGGLE: 'start-toggle',
+  START_HOURS: 'start-hours',
+  START_MINUTES: 'start-minutes',
+  END_TOGGLE: 'end-toggle',
+  END_HOURS: 'end-hours',
+  END_MINUTES: 'end-minutes',
+};
 const END_TIME_OVER_MSG = '시작 시간보다 종료시간은 빠를 수 없습니다.';
+const DATE_RANGE_LIMITS = {
+  START: new Date(0),
+  END: new Date(2999, 12, 31),
+};
 
-const headerCloseBtns = [...document.querySelectorAll('.header__close-btn')];
-const mainLayout = document.querySelector('.js-main-layout');
-const categoryLayout = document.querySelector('.js-category-layout');
-
+// Create an instance of Notyf
+const notyf = new Notyf({
+  duration: 2000,
+  position: {
+    x: 'center',
+    y: 'bottom',
+  },
+  types: [
+    {
+      type: 'info',
+      background: '#323232',
+      icon: false,
+    },
+  ],
+});
 const detailObjs = [];
+
+const toast = (msg) => {
+  notyf.open({
+    type: 'info',
+    message: msg,
+  });
+};
 
 const toggleLayout = () => {
   const isLayoutMain = document.body.dataset.layout === LAYOUT_NAME.MAIN;
@@ -54,7 +99,7 @@ const limitFileSize = (file) => {
   const isOver = file.size > FILE_SIZE_LIMIT;
 
   if (isOver) {
-    alert(FILE_SIZE_OVER_MSG);
+    toast(FILE_SIZE_OVER_MSG);
   }
 
   return isOver;
@@ -96,22 +141,60 @@ const updateSelectBoxValue = (selectBox, value) => {
 const filterValueNumberOnly = (value) => value.replace(/[^0-9]/g, '');
 
 const getDetailItems = () => [...document.querySelectorAll('.js-detail-item')];
-// const updateDetailItemIndex = (detailItem, index) => {
-//   const numberEl = detailItem.querySelector('.js-detail-item__no');
 
-//   if (!numberEl) return;
+const updateDetailObjsDatePickers = () => {
+  if (detailObjs.length === 1) return;
 
-//   numberEl.innerText = index + 1;
-// };
-const updateDetailItems = () => {
-  // const detailItems = detailItems;
-  // const detailItemLength = detailObjs.length;
-  // const useDeleteBtn = detailItemLength > 1;
-  // detailItems.forEach((detailItem, index) => {
-  //   const btn = detailItem.querySelector('.js-delete-detail-item');
-  //   useDeleteBtn ? showElem(btn) : hideElem(btn);
-  //   updateDetailItemIndex(detailItem, index);
-  // });
+  detailObjs.forEach((detailObj, index) => {
+    const indexDiffs = { prev: null, next: null };
+    const selectedNearbyDetailObj = {
+      prev: detailObjs.findLast((detailObj, _index) => {
+        if (_index < index && detailObj.isDateSelected) {
+          indexDiffs.prev = index - _index;
+
+          return true;
+        }
+      }),
+      next: detailObjs.find((detailObj, _index) => {
+        if (_index > index && detailObj.isDateSelected) {
+          indexDiffs.next = _index - index;
+
+          return true;
+        }
+      }),
+    };
+    const days = {
+      get prev() {
+        if (!selectedNearbyDetailObj.prev) return DATE_RANGE_LIMITS.START;
+
+        return addDays(
+          selectedNearbyDetailObj.prev.selectedDate,
+          indexDiffs.prev,
+        );
+      },
+      get next() {
+        if (!selectedNearbyDetailObj.next) return DATE_RANGE_LIMITS.END;
+
+        return subDays(
+          selectedNearbyDetailObj.next.selectedDate,
+          indexDiffs.next,
+        );
+      },
+    };
+    const needToSetRange =
+      selectedNearbyDetailObj.prev || selectedNearbyDetailObj.next;
+    const needToSetDate = needToSetRange && !detailObj.isDateSelected;
+
+    if (needToSetRange) {
+      const ranges = [[days.prev, days.next]];
+      detailObj.setDatePickerRanges(ranges);
+    }
+
+    if (needToSetDate) {
+      const dateToSet = selectedNearbyDetailObj.prev ? days.prev : days.next;
+      detailObj.setDatePickerDate(dateToSet);
+    }
+  });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -212,7 +295,7 @@ function moreImages() {
     const emptyImgElLength = getEmptyImgEls().length;
     const isFilesOverEmptyImgElLength = input.files.length > emptyImgElLength;
 
-    if (isFilesOverEmptyImgElLength) return alert(MORE_FILES_OVER_LENGTH_MSG);
+    if (isFilesOverEmptyImgElLength) return toast(MORE_FILES_OVER_LENGTH_MSG);
 
     for (const file of input.files) {
       const isSizeOver = limitFileSize(file);
@@ -257,6 +340,7 @@ function initCategorySection() {
 
   const selectBox = categorySection.querySelector('.select-box');
   const btns = [...categorySelectSection.querySelectorAll('.btn')];
+  const headerCloseBtns = [...document.querySelectorAll('.header__close-btn')];
 
   const getBtnValue = (btn) => btn.dataset.value;
   const getActiveBtns = () => btns.filter(validIsBtnActive);
@@ -289,8 +373,7 @@ function initCategorySection() {
     const isActiveBtnsOver = activeBtns.length === CATEGORY_BTN_ACTIVE_LIMIT;
 
     if (isActiveBtnsOver) {
-      // TO-DO: Toast로 교체
-      alert(CATEGORY_ACTIVE_OVER_MGS);
+      toast(CATEGORY_ACTIVE_OVER_MGS);
 
       return;
     }
@@ -365,8 +448,7 @@ function initDetails() {
       detailObj.updateDeleteBtn();
       detailObj.updateNumberElement(index + 1);
     });
-
-    console.log('appendDetailItem', { detailObjs });
+    updateDetailObjsDatePickers();
   };
 
   if (!detailItemsConainer || !addDetailBtn) return;
@@ -379,156 +461,320 @@ function initDetails() {
 function initDetailItem(detailItem) {
   if (!detailItem) return;
 
-  const deleteBtn = detailItem.querySelector('.js-delete-detail-item');
-  const numberElement = detailItem.querySelector('.js-detail-item__no');
-  const dateSelectBox = detailItem.querySelector('.js-details-select-date-box');
-  const datePickerInput = dateSelectBox.querySelector('.js-date-picker-input');
-  const datePickerWrapper = detailItem.querySelector('.js-date-picker-wrapper');
   const amPmToggles = [...detailItem.querySelectorAll('.js-am-pm-toggle ')];
   const inputs = [...detailItem.querySelectorAll('input')];
   const textarea = detailItem.querySelector('.js-textarea');
 
-  const deleteThis = () => {
-    const targetIndex = detailObjs.findIndex((detailObj) =>
-      detailObj.validIsElement(detailItem),
-    );
-    detailObjs.splice(targetIndex, 1);
-    detailItem.remove();
-    detailObjs.forEach((detailObj, index) => {
-      detailObj.updateDeleteBtn();
-      detailObj.updateNumberElement(index + 1);
+  const { updateDeleteBtn } = (function initDeleteBtn() {
+    const deleteBtn = detailItem.querySelector('.js-delete-detail-item');
+
+    if (!deleteBtn) return;
+
+    deleteBtn.addEventListener('click', () => {
+      const targetIndex = detailObjs.findIndex((detailObj) =>
+        detailObj.validIsElement(detailItem),
+      );
+      detailObjs.splice(targetIndex, 1);
+      detailItem.remove();
+      detailObjs.forEach((detailObj, index) => {
+        detailObj.updateDeleteBtn();
+        detailObj.updateNumberElement(index + 1);
+      });
     });
-  };
 
-  const findAmPmToggle = (isStart) =>
-    amPmToggles.find((toggle) =>
-      isStart
-        ? toggle.className.includes('start')
-        : toggle.className.includes('end'),
+    return {
+      updateDeleteBtn() {
+        const useDeleteBtn = detailObjs.length > 1;
+
+        useDeleteBtn ? showElem(deleteBtn) : hideElem(deleteBtn);
+      },
+    };
+  })();
+
+  const { datepicker, getIsDateSelected } = (function initdateSelectBox() {
+    const dateSelectBox = detailItem.querySelector(
+      '.js-details-select-date-box',
     );
+    const datePickerInput = dateSelectBox.querySelector(
+      '.js-date-picker-input',
+    );
+    const datePickerWrapper = detailItem.querySelector(
+      '.js-date-picker-wrapper',
+    );
+    const datepicker = new DatePicker(datePickerWrapper, {
+      date: new Date(),
+      language: 'ko',
+      input: {
+        element: datePickerInput,
+        format: 'yyyy년 M월 d일',
+      },
+      openers: [dateSelectBox],
+      autoClose: false,
+    });
+    let isDateSelected = false;
+    let latestDate;
 
-  const findInput = (isStart, isHour) => {
-    const name = `${isStart ? 'start' : 'end'}-${isHour ? 'hour' : 'min'}`;
+    datepicker.on('open', () => {
+      latestDate = datepicker.getDate();
+    });
 
-    return inputs.find((input) => input.name === name);
-  };
+    datepicker.on('close', () => {
+      datepicker.setDate(latestDate);
+    });
 
-  const handleAmPmToggle = (event) => {
-    const { currentTarget: toggle } = event;
-    const isAm = toggle.dataset.value === 'am';
-    const nextValue = isAm ? 'pm' : 'am';
+    const changeDatePickerBtn = document.createElement('button');
+    changeDatePickerBtn.className =
+      'change-date-picker-btn btn btn--sm btn--primary';
+    changeDatePickerBtn.textContent = '선택 완료';
+    datePickerWrapper
+      .querySelector('.tui-datepicker')
+      .append(changeDatePickerBtn);
+    changeDatePickerBtn.addEventListener('click', () => {
+      isDateSelected = true;
+      latestDate = datepicker.getDate();
 
-    toggle.dataset.value = nextValue;
-    toggle.innerText = AM_PM_LABELS[nextValue];
-  };
+      updateSelectBoxValue(dateSelectBox, datePickerInput.value);
+      updateDetailObjsDatePickers();
+      datepicker.close();
+    });
 
-  const handleInputInput = (event) => {
-    const { currentTarget: input } = event;
+    return {
+      datepicker,
+      getIsDateSelected() {
+        return isDateSelected;
+      },
+    };
+  })();
 
-    input.value = filterValueNumberOnly(input.value);
-
-    const { name } = input;
-    const isStart = name.includes('start');
-    const isHour = name.includes('hour');
-
-    if (isHour && parseInt(input.value) > 12) {
-      input.value = '12';
-    }
-
-    if (!isHour && parseInt(input.value) > 59) {
-      input.value = '59';
-    }
-
-    const inputs = {
+  (function initTimeSelects() {
+    const lastValues = {
       start: {
-        amPm: findAmPmToggle(true),
-        hour: findInput(true, true),
-        min: findInput(true, false),
+        amPm: 'am',
+        hours: '10',
+        minutes: '00',
       },
       end: {
-        amPm: findAmPmToggle(false),
-        hour: findInput(true, true),
-        min: findInput(true, false),
+        amPm: 'am',
+        hours: '11',
+        minutes: '00',
       },
     };
 
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDate();
-    const startInputs = {
-      amPm: findAmPmToggle(true),
-      hour: findInput(true, true),
-      min: findInput(true, false),
-    };
-    const endInputs = {
-      amPm: findAmPmToggle(false),
-      hour: findInput(true, true),
-      min: findInput(true, false),
+    const findAmPmToggle = (startEnd) =>
+      amPmToggles.find((toggle) => toggle.className.includes(startEnd));
+
+    const findInput = (startEnd, hourMin) => {
+      const name = `${startEnd}-${hourMin}`;
+
+      return inputs.find((input) => input.name === name);
     };
 
-    console.log({ isStart, isHour, inputs });
-  };
+    const findStartEndInputs = (startEnd) => {
+      const amPmToggle = findAmPmToggle(startEnd);
+      const hoursInput = findInput(startEnd, 'hour');
+      const minutesInput = findInput(startEnd, 'min');
 
-  if (!deleteBtn || !dateSelectBox || !amPmToggles.length || !inputs.length)
-    return;
+      return { amPmToggle, hoursInput, minutesInput };
+    };
 
-  deleteBtn.addEventListener('click', deleteThis);
+    const formatDateToHoursMinutes = (date) => format(date, 'H-mm').split('-');
 
-  DatePicker.localeTexts['ko'] = {
-    ...DatePicker.localeTexts['ko'],
-    titleFormat: 'yyyy년 MM월',
-  };
+    const getStartEndDate = (startEnd) => {
+      const { amPmToggle, hoursInput, minutesInput } =
+        findStartEndInputs(startEnd);
+      const isPm = amPmToggle.dataset.value === 'pm';
+      const hours = (isPm ? 12 : 0) + parseInt(hoursInput.value);
+      const minutes = parseInt(minutesInput.value);
+      const date = setMinutes(setHours(new Date(), hours), minutes);
 
-  const datepicker = new DatePicker(datePickerWrapper, {
-    date: new Date(),
-    language: 'ko',
-    input: {
-      element: datePickerInput,
-      format: 'yyyy년 MM월 dd일',
-    },
-    openers: [dateSelectBox],
-    autoClose: false,
-  });
+      return date;
+    };
 
-  datepicker.on('change', () => {
-    console.log(`Selected date: ${datepicker.getDate()}`);
-    console.log(
-      datePickerWrapper.querySelector('.tui-calendar-title-month').innerHTML,
-    );
-  });
+    const setStartEndDate = (startEnd, date) => {
+      console.log('setStartEndDate', { startEnd, date });
+      const { amPmToggle, hoursInput, minutesInput } =
+        findStartEndInputs(startEnd);
+      const [hours, minutes] = formatDateToHoursMinutes(date);
 
-  // datepicker.setRanges([[new Date(2025, 11, 10), new Date(2025, 12, 26)]]);
+      setAmPmToggle(amPmToggle, hours >= 12 ? 'pm' : 'am');
+      hoursInput.value = `${hours % 12}`;
+      minutesInput.value = `${minutes}`;
+    };
 
-  const changeDatePickerBtn = document.createElement('button');
-  changeDatePickerBtn.className =
-    'change-date-picker-btn btn btn--sm btn--primary';
-  changeDatePickerBtn.textContent = '선택 완료';
-  datePickerWrapper
-    .querySelector('.tui-datepicker')
-    .append(changeDatePickerBtn);
+    const setAmPmToggle = (toggle, amPm) => {
+      toggle.dataset.value = amPm;
+      toggle.innerText = AM_PM_LABELS[amPm];
+    };
 
-  for (const toggle of amPmToggles) {
-    toggle.addEventListener('click', handleAmPmToggle);
-  }
+    const updateLastValues = () => {
+      const startDate = getStartEndDate('start');
+      const [startHours, startMinutes] = formatDateToHoursMinutes(startDate);
+      const endDate = getStartEndDate('end');
+      const [endHours, endMinutes] = formatDateToHoursMinutes(endDate);
 
-  for (const input of inputs) {
-    input.addEventListener('input', handleInputInput);
-  }
+      lastValues.start.amPm = startHours < 12 ? 'am' : 'pm';
+      lastValues.start.hours = `${startHours % 12}`;
+      lastValues.start.minutes = `${startMinutes}`;
+      lastValues.end.amPm = endHours < 12 ? 'am' : 'pm';
+      lastValues.end.hours = `${endHours % 12}`;
+      lastValues.end.minutes = `${endMinutes}`;
+    };
+
+    const updateStartEnd = (trigger) => {
+      const startDate = getStartEndDate('start');
+      const endDate = getStartEndDate('end');
+      const isStartOverEnd =
+        isAfter(startDate, endDate) || isEqual(startDate, endDate);
+
+      if (!isStartOverEnd) {
+        updateLastValues();
+
+        return;
+      }
+
+      const isStart = [
+        UPDATE_TIME_TRIGGERS.START_TOGGLE,
+        UPDATE_TIME_TRIGGERS.START_HOURS,
+        UPDATE_TIME_TRIGGERS.START_MINUTES,
+      ].includes(trigger);
+
+      if (isStart) {
+        const newEndDate = clamp(addHours(startDate, 1), {
+          start: setMinutes(setHours(startDate, 0), 0),
+          end: setMinutes(setHours(startDate, 23), 59),
+        });
+
+        setStartEndDate('end', newEndDate);
+      }
+
+      if (trigger === UPDATE_TIME_TRIGGERS.END_TOGGLE) {
+        const endAmPmToggle = findAmPmToggle('end');
+
+        setAmPmToggle(endAmPmToggle, lastValues.end.amPm);
+        toast(END_TIME_OVER_MSG);
+
+        return;
+      }
+
+      if (trigger === UPDATE_TIME_TRIGGERS.END_HOURS) {
+        const endHourInput = findInput('end', 'hour');
+
+        endHourInput.value = lastValues.end.hours;
+        toast(END_TIME_OVER_MSG);
+
+        return;
+      }
+
+      if (trigger === UPDATE_TIME_TRIGGERS.END_MINUTES) {
+        const endMinInput = findInput('end', 'min');
+
+        endMinInput.value = lastValues.end.minutes;
+        toast(END_TIME_OVER_MSG);
+
+        return;
+      }
+
+      updateLastValues();
+
+      console.log({ startDate, endDate });
+    };
+
+    const handleAmPmToggle = (event) => {
+      const { currentTarget: toggle } = event;
+      const isStart = toggle.classList.contains('start');
+      const isAm = toggle.dataset.value === 'am';
+      const nextValue = isAm ? 'pm' : 'am';
+      const updateTrigger = isStart
+        ? UPDATE_TIME_TRIGGERS.START_TOGGLE
+        : UPDATE_TIME_TRIGGERS.END_TOGGLE;
+
+      setAmPmToggle(toggle, nextValue);
+      updateStartEnd(updateTrigger);
+    };
+
+    const handleInput = (event) => {
+      const { currentTarget: input } = event;
+
+      if (input.value === '') return;
+
+      input.value = filterValueNumberOnly(input.value);
+
+      const { name } = input;
+      const isStart = name.includes('start');
+      const isHour = name.includes('hour');
+
+      if (isHour && parseInt(input.value) > 11) {
+        input.value = '11';
+      }
+
+      if (!isHour && parseInt(input.value) > 59) {
+        input.value = '59';
+      }
+
+      const trigger = isStart
+        ? isHour
+          ? UPDATE_TIME_TRIGGERS.START_HOURS
+          : UPDATE_TIME_TRIGGERS.START_MINUTES
+        : isHour
+          ? UPDATE_TIME_TRIGGERS.END_HOURS
+          : UPDATE_TIME_TRIGGERS.END_MINUTES;
+
+      updateStartEnd(trigger);
+    };
+
+    const handleBlur = (event) => {
+      const { currentTarget: input } = event;
+
+      if (input.value !== '') return;
+
+      const { name } = input;
+      const isStart = name.includes('start');
+      const isHour = name.includes('hour');
+      const lastValue = isStart
+        ? isHour
+          ? lastValues.start.hours
+          : lastValues.start.minutes
+        : isHour
+          ? lastValues.end.hours
+          : lastValues.end.minutes;
+
+      input.value = lastValue;
+    };
+
+    for (const toggle of amPmToggles) {
+      toggle.addEventListener('click', handleAmPmToggle);
+    }
+
+    for (const input of inputs) {
+      input.addEventListener('input', handleInput);
+      input.addEventListener('blur', handleBlur);
+    }
+  })();
 
   initTextarea(textarea);
 
   return {
-    updateDeleteBtn() {
-      const useDeleteBtn = detailObjs.length > 1;
-
-      useDeleteBtn ? showElem(deleteBtn) : hideElem(deleteBtn);
+    get selectedDate() {
+      return datepicker.getDate();
     },
+    get isDateSelected() {
+      return getIsDateSelected();
+    },
+
+    updateDeleteBtn,
     updateNumberElement(index) {
+      const numberElement = detailItem.querySelector('.js-detail-item__no');
+
       numberElement.innerText = index;
     },
     validIsElement(element) {
       return element.isEqualNode(detailItem);
+    },
+    setDatePickerRanges(ranges) {
+      datepicker.setRanges(ranges);
+    },
+    setDatePickerDate(date) {
+      datepicker.setDate(date);
     },
   };
 }
@@ -536,7 +782,6 @@ function initDetailItem(detailItem) {
 function initTextarea(textarea) {
   const wrapper = textarea.closest('.textarea-wrapper');
   const count = wrapper.querySelector('.js-textarea__count');
-  let isFocused = false;
 
   const setState = (state) => {
     wrapper.dataset.state = state;
@@ -558,21 +803,16 @@ function initTextarea(textarea) {
   };
 
   textarea.addEventListener('focus', () => {
-    isFocused = true;
-
     validate();
   });
 
   textarea.addEventListener('blur', () => {
-    isFocused = false;
-
     if (wrapper.dataset.state === 'success') {
       wrapper.dataset.state = '';
     }
   });
 
   textarea.addEventListener('input', () => {
-    console.log('input');
     preventMoreThanTwoSpaces();
     validate();
     count.innerHTML = `${textarea.value.length}`;
